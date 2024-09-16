085dc77e9680439ba8c48c8d2ddea71eb7806c65
Abhishek <averma8534@gmail.com>
2024-09-16 13:24:28 +0530

    commit

--- 2nd day/float_space.c
+++ 2nd day/float_space.c
@@ -4,6 +4,6 @@ int main()
 {
     int a=34;
     float b=4.5432;
-    printf("%34d%18.5f",a,b);// this adds spaces %(number)f,d,c,l(long),lf(double),Lf(long double)
+    printf("%34d%d",a,b);// this adds spaces %(number)f,d,c,l(long),lf(double),Lf(long double)
     return 0;
 }
